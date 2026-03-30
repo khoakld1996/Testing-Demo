1bbc969887b9a947886501ce261048d42f68a542
khoakld1996 <khoakld1996@gmail.com>
2026-03-30 16:55:05 +0700

    Summary

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ===== API GOOGLE SCRIPT =====
-const API = "https://script.google.com/macros/s/AKfycbyX7PCWMwmKw86Iv3fsGX8vwTMYOCovEupXgxbzKLP7kSwMIoiVOfiQ1c60IR3tc4Hr/exec";
+const API = "https://script.google.com/macros/s/AKfycbyf4LJfPW0pXSpegPOg03WxmtaebP6WnTwo3If2tLPr8NltxPyqd5LgtCtHgnq8Pxi4/exec";
 
 // ================= GLOBAL =================
 let questions = [];
